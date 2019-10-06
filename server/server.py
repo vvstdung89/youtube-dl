941c237fc7b99f800f4373f4eb712ed3f6463e14
DungVan <vvstdung89@gmail.com>
2019-10-06 13:07:25 +0700

    update

--- server/server.py
+++ server/server.py
@@ -4,24 +4,46 @@ from flask import Flask, json, request
 
 from youtube_dl import YoutubeDL
 
-api = Flask(__name__)
-# ydl = YoutubeDL({'nocheckcertificate': True, 'youtube_include_dash_manifest': False, 'proxy': "http://localhost:1080"})
+YTValids = ["18", "22", "250", "249", "160", "133", "243", "395"]
 
-@api.route('/get', methods=['GET'])
-def get_video():
-    start_time = time.time()
-    ydl = YoutubeDL(
-        {'nocheckcertificate': True, 'youtube_include_dash_manifest': False})
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
+api = Flask(__name__)
+
+
+@api.route('/youtube', methods=['GET'])
+def get_youtube():
     id = request.args.get('id')
+    proxy = request.args.get('proxy')
+
     if id == "":
         return ""
+
+    if proxy != "":
+        youtubeConfig = {'nocheckcertificate': True, 'youtube_include_dash_manifest': False}
+
+    else:
+        youtubeConfig = {'nocheckcertificate': True, 'youtube_include_dash_manifest': False, 'proxy': proxy}
+
+
     start_time = time.time()
+    ydl = YoutubeDL(youtubeConfig)
     r = ydl.extract_info("https://www.youtube.com/watch?v=" + id, False, "Youtube")
     elapsed_time = time.time() - start_time
     print(elapsed_time)
-    return json.dumps(r)
+
+
+    formats = {}
+    for item in r['formats']:
+        itag = item['format_id']
+        if itag in YTValids:
+            formats[itag] = item['url']
+
+    return json.dumps({
+        'title': r['title'],
+        'thumbnail': r['thumbnail'],
+        'duration': r['duration'],
+        'formats': formats,
+        'is_live': r['is_live']
+    })
 
 
 if __name__ == '__main__':
