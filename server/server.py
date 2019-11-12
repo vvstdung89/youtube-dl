16cdee36eece4da421b920efafce1a7ebbba37a6
DungVan <vvstdung89@gmail.com>
2019-11-12 18:27:43 +0700

    add try catch

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import threading
 
 import os
 import sys
+
 sys.path.append(os.getcwd())
 
 from youtube_dl import YoutubeDL
@@ -12,50 +13,68 @@ from youtube_dl import YoutubeDL
 YTValids = ["18", "22", "43", "243", "247", "248", "271",
             "313", "272", "139", "140", "141", "249", "250", "251"]
 
+
 def on_request(ch, method, props, body):
+    try:
+        jsonObj = json.loads(body)
+        print(jsonObj)
 
-    jsonObj = json.loads(body)
-    print(jsonObj)
+        if "proxy" not in jsonObj:
+            youtubeConfig = {'nocheckcertificate': True,
+                             'youtube_include_dash_manifest': False}
+        else:
+            youtubeConfig = {'nocheckcertificate': True,
+                             'youtube_include_dash_manifest': False, 'proxy': jsonObj["proxy"]}
 
-    if "proxy" not in jsonObj:
-        youtubeConfig = {'nocheckcertificate': True,
-                         'youtube_include_dash_manifest': False}
-    else:
-        youtubeConfig = {'nocheckcertificate': True,
-                         'youtube_include_dash_manifest': False, 'proxy': jsonObj["proxy"]}
+        start_time = time.time()
+        ydl = YoutubeDL(youtubeConfig)
+        r = ydl.extract_info(
+            "https://www.youtube.com/watch?v=" + jsonObj['id'], False, "Youtube")
+        elapsed_time = time.time() - start_time
+        print(elapsed_time)
 
-    start_time = time.time()
-    ydl = YoutubeDL(youtubeConfig)
-    r = ydl.extract_info(
-        "https://www.youtube.com/watch?v=" + jsonObj['id'], False, "Youtube")
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
+        formats = []
+        for item in r['formats']:
+            itag = item['format_id']
+            if itag in YTValids:
+                format = {
+                    'itag': itag,
+                    'url': item['url'],
+                    'size': item['filesize'],
+                    'ext': item['ext'],
+                }
+                formats.append(format)
 
-    formats = []
-    for item in r['formats']:
-        itag = item['format_id']
-        if itag in YTValids:
-            format = {
-                'itag': itag,
-                'url': item['url'],
-                'size': item['filesize'],
-                'ext': item['ext'],
-            }
-            formats.append(format)
+        res = json.dumps({
+            'type': "youtube",
+            'id': jsonObj['id'],
+            'title': r['title'],
+            'thumbnail': r['thumbnail'],
+            'duration': r['duration'],
+            'formats': formats,
+        })
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(
+                             correlation_id=props.correlation_id),
+                         body=str(res))
 
-    res = json.dumps({
-        'title': r['title'],
-        'thumbnail': r['thumbnail'],
-        'duration': r['duration'],
-        'formats': formats,
-    })
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(
-                         correlation_id=props.correlation_id),
-                     body=str(res))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception:
+        res = json.dumps({
+            'type': "youtube",
+            'id': jsonObj['id'],
+            'error': True,
+        })
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+        ch.basic_publish(exchange='',
+                         routing_key=props.reply_to,
+                         properties=pika.BasicProperties(
+                             correlation_id=props.correlation_id),
+                         body=str(res))
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 
 def run():
